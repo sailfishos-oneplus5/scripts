49948750c41acfbe2df7a239b7bf9d269a9847da
Jami Kettunen <jami.kettunen@protonmail.com>
2019-11-12 20:30:15 +0200

    fixup-helper: Initial script adaptation for Halium trees

--- fixup-helper.sh
+++ fixup-helper.sh
@@ -1,12 +1,14 @@
 #!/usr/bin/env bash
-# fixup-helper.sh — A quick way to generate fixup-mountpoints for devices from PLATFORM_SDK
+# fixup-helper.sh — A quick way to generate fixup-mountpoints for devices
+
+# Variables (user defined)
+HALIUM_ROOT="$HOME/halium"
 
 # Variables (set on runtime)
-version_id="" # e.g. '3.1.0.12'
 fstab_file="" # e.g. '.../device/.../fstab.qcom'
 par_prefix="" # e.g. '(/dev/)block/bootdevice/by-name'
 boardname=""  # e.g. 'cheeseburger'
-final_file="" # e.g. '.../hybris/hybris-boot/mountpoints-for-cheeseburger.txt'
+final_file="" # e.g. '.../halium/hybris-boot/mountpoints-for-cheeseburger.txt'
 partitions="" # e.g. 'lrwxrwxrwx 1 root root 16 1972-04-12 07:45 LOGO -> /dev/block/sde18\n...'
 
 # Functions
@@ -14,46 +16,34 @@ msg() { echo "fixup-helper: $@"; }
 append() { echo "$@" >> "$final_file"; }
 
 # 1. Make sure we start in the correct dir
-if ! cd "$ANDROID_ROOT" &> /dev/null; then
-	msg "ERROR: \$ANDROID_ROOT was not set!"
-	msg "Please check your configuration files and (re-)enter PLATFORM_SDK"
+if ! cd "$HALIUM_ROOT" &> /dev/null; then
+	msg "ERROR: Couldn't enter \$HALIUM_ROOT ('$HALIUM_ROOT')!"
+	msg "Please re-configure the script variable!"
 	exit 1
 fi
 
-# 2. Make sure we're in PLATFORM_SDK env
-if ! cat /etc/os-release | grep -q Sailfish; then
-	msg "ERROR: You are not in the PLATFORM_SDK environment!"
-	exit 2
-fi
-
-version_id=`cat /etc/os-release | grep VERSION_ID | cut -d'=' -f2`
-msg "PLATFORM_SDK env for SFOS $version_id detected"
-
-# 3. Make sure we have ADB
+# 2. Make sure we have ADB
 if ! which adb &> /dev/null; then
 	# TODO: Override for e.g. 'ls -l /dev/block/bootdevice/by-name/' output from device via arg/file?
-	sudo zypper --non-interactive in android-tools-hadk &> /dev/null
-	if ! which adb &> /dev/null; then
-		msg "ERROR: Couldn't install missing 'android-tools-hadk' package!"
-		exit 3
-	fi
+	msg "ERROR: Please install package containing currently missing 'adb' binary!"
+	exit 3
 fi
 
 fstab_file=`find device/ -name fstab* | head -1`
 
-# 4. Find device fstab file
+# 3. Find device fstab file
 if [ -z "$fstab_file" ]; then
 	# TODO: Override for partition prefix via arg/file?
-	msg "ERROR: A device fstab file could not be found in '$ANDROID_ROOT/device/'!"
+	msg "ERROR: A device fstab file could not be found in '$HALIUM_ROOT/device/'!"
 	msg "Have you synced all your sources?"
 	exit 4
 fi
 
 msg "Using '$fstab_file' as the device fstab file"
-fstab_file="$ANDROID_ROOT/$fstab_file"
+fstab_file="$HALIUM_ROOT/$fstab_file"
 par_prefix=`cat "$fstab_file" | grep "/dev\/block" | head -1 | xargs | cut -d' ' -f1 | sed 's/\(.*\)\/.*/\1/'`
 
-# 5. Get proper prefix for partitions
+# 4. Get proper prefix for partitions
 if [ -z "$par_prefix" ]; then
 	# TODO: Override for partition prefix via arg/file?
 	msg "ERROR: Couldn't figure out partition prefix from fstab!"
@@ -62,7 +52,7 @@ fi
 
 msg "Prefix for partitions from fstab: '$par_prefix'"
 
-# 6. Wait for a device in recovery mode...
+# 5. Wait for a device in recovery mode...
 msg "Waiting for a device in recovery mode..."
 while true; do
 	devices_list=`adb devices`
@@ -83,7 +73,7 @@ while true; do
 	fi
 done
 
-# 7. Attempt to fetch device boardname
+# 6. Attempt to fetch device boardname
 # TODO: Use $DEVICE from ~/.hadk.env?
 # TODO: Override for boardname via arg/file?
 boardname=`adb shell "getprop ro.product.device"`
@@ -91,14 +81,14 @@ boardname=`adb shell "getprop ro.product.device"`
 
 msg "Found your $boardname in recovery mode, building list of mountpoints..."
 
-# 8. Get all device partitions
+# 7. Get all device partitions
 partitions=`adb shell "ls -l $par_prefix/" | sed '1d'`
 
 # TODO: Make sure we've found the correct pseudo-node for device partitions?
 
-# 9. Generate output file with fixup-mountpoints for the device
+# 8. Generate output file with fixup-mountpoints for the device
 par_prefix="${par_prefix:5}" # Remove '/dev/' part for fixup-mountpoints
-final_file="$ANDROID_ROOT/hybris/hybris-boot/mountpoints-for-$boardname.txt"
+final_file="$HALIUM_ROOT/halium/hybris-boot/mountpoints-for-$boardname.txt"
 rm -f "$final_file"
 
 # BEGIN
@@ -120,5 +110,5 @@ append "            \"\$@\""
 append "        ;;"
 append
 
-msg "We're done here; mountpoints for your device can be found in '\$ANDROID_ROOT/hybris/hybris-boot/mountpoints-for-$boardname.txt'"
-msg "Place the generated mountpoints in '\$ANDROID_ROOT/hybris/hybris-boot/fixup-mountpoints' among all other devices"
+msg "We're done here; mountpoints for your device can be found in '\$HALIUM_ROOT/halium/hybris-boot/mountpoints-for-$boardname.txt'"
+msg "Place the generated mountpoints in '\$HALIUM_ROOT/halium/hybris-boot/fixup-mountpoints' among all other devices"
